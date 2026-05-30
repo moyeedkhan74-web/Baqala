9fc254f11079134d35e8b3d8a05ddd0a84d629a0
moyeed <moyeedkhan74@gmail.com>
2026-05-30 22:41:55 +0530

    debug: fix 'not a function' error and relax CORS for connectivity test

--- server/server.js
+++ server/server.js
@@ -40,16 +40,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl) 
-    // unless you want to be extremely strict
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true, // Allow all origins temporarily for debug
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
