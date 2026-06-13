b3de3b8b2a7afbe2461fffc8db5a78db99a5415b
moyeed <moyeedkhan74@gmail.com>
2026-06-13 23:56:59 +0530

    chore: clean up diagnostic scripts and finalize fixes

--- test-api.js
+++
@@ -1,7 +0,0 @@
-const https = require('https');
-
-https.get('https://baqala.onrender.com/api/apps?featured=true', (res) => {
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => console.log(data));
-});
